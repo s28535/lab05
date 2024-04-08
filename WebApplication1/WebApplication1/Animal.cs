0f7ac3a0bcc7d7d23fb766099cafb25e235a8853
s28535 <s28535@pjwstk.edu.pl>
2024-04-08 13:38:55 +0200

    xd

---
+++ WebApplication1/WebApplication1/Animal.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+
+namespace WebApplication1;
+
+public class Animal
+{
+    private int Id { get; }
+    private string Name { get; set; }
+    private string Category { get; set; }
+    private double Mass { get; set; }
+    private Color Color { get; set; }
+
+    public Animal(int id, string name, string category, double mass, Color color)
+    {
+        Id = id;
+        Name = name;
+        Category = category;
+        Mass = mass;
+        Color = color;
+    }
+}
---
+++ WebApplication1/WebApplication1/Visit.cs
@@ -0,0 +1,19 @@
+﻿namespace WebApplication1;
+
+public class Visit
+{
+    private DateTime Date { get; set; }
+    private Animal Animal { get; set; }
+    private string Description { get; set; }
+    private double Price { get; set; }
+
+    public Visit(DateTime date, Animal animal, string description, double price)
+    {
+        Date = date;
+        Animal = animal;
+        Description = description;
+        Price = price;
+    }
+    
+    
+}
